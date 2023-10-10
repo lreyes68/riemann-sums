6c67d4277cc952dc4a8c401f2cb71ec5077b9717
that1mexicanguy <59989206+that1mexicanguy@users.noreply.github.com>
2023-10-10 14:25:27 -0700

    Created calculations for trapezoidal riemann sum, given a set amount of subintervals.

--- main.cpp
+++ main.cpp
@@ -67,13 +67,9 @@ int midpointRiemann () {
         heightNum1 = lower + (steps * i);
         temp = heightNum1;
         heightNum2 = temp + steps;
-
-        cout << "Height 1: "<< heightNum1 << endl;
-        cout << "Height 2: " << heightNum2 << endl;
+        
 
         midpoint = (heightNum1 + heightNum2) / 2;
-
-        cout << "Midpoint: " << midpoint << endl;
 
         height = pow(midpoint, 2);
 
@@ -84,8 +80,39 @@ int midpointRiemann () {
 
     return 0;
 }
+
+int trapezoidRiemann () {
+    float upper, lower, stepNumber, steps;
+
+
+    cin >> upper >> lower >> stepNumber;
+    
+    steps = (upper - lower) / stepNumber;
+
+    double heightNum, height, sum1, sum2;
+    for (int i = 0; i < stepNumber+1; i++) {
+        if (i == 0 || i == 40) {
+            heightNum = lower + (steps * i );
+            height = pow(heightNum, 2);
+
+            sum1 += height;
+
+        }else {
+            heightNum = lower + (steps * i);
+            height = (pow(heightNum, 2)) * 2;
+
+            sum2 += height;
+            
+        }
+    }
+    double sum, area;
+    sum = sum1 + sum2;
+    area = sum * (steps / 2);
+    cout << area << endl;
+    return 0;
+}
 int main() {
    
-    midpointRiemann();
+    trapezoidRiemann();
 
 }
