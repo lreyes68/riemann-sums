92da5785da9d488f3bd1c58dc78e63262da5486f
that1mexicanguy <59989206+that1mexicanguy@users.noreply.github.com>
2023-10-10 14:00:04 -0700

    Created calculations for midpoint riemann sum, with a given number of subintervals.

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ int rightRiemann () {
 }
 
 int leftRiemann () {
-     float upper, lower, stepNumber, steps;
+    float upper, lower, stepNumber, steps;
 
 
     cin >> upper >> lower >> stepNumber;
@@ -53,8 +53,39 @@ int leftRiemann () {
    
 }
 
+int midpointRiemann () {
+    float upper, lower, stepNumber, steps;
+
+
+    cin >> upper >> lower >> stepNumber;
+    
+    steps = (upper - lower) / stepNumber;
+
+    double heightNum1,heightNum2, midpoint, temp, height, sum;
+    for (int i = 0; i <stepNumber; i++) {
+
+        heightNum1 = lower + (steps * i);
+        temp = heightNum1;
+        heightNum2 = temp + steps;
+
+        cout << "Height 1: "<< heightNum1 << endl;
+        cout << "Height 2: " << heightNum2 << endl;
+
+        midpoint = (heightNum1 + heightNum2) / 2;
+
+        cout << "Midpoint: " << midpoint << endl;
+
+        height = pow(midpoint, 2);
+
+        sum += height;
+    }
+    double area = sum * steps;
+    cout << area << endl;
+
+    return 0;
+}
 int main() {
    
-    leftRiemann();
+    midpointRiemann();
 
 }
