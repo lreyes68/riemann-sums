cdbb42bc319fc435be6b0068ffa95ebf7ad38d8c
that1mexicanguy <59989206+that1mexicanguy@users.noreply.github.com>
2023-10-10 13:38:53 -0700

    Created calculations for a left riemann sum, given a set of subintervals.

--- main.cpp
+++ main.cpp
@@ -28,8 +28,33 @@ int rightRiemann () {
     return 0;
 
 }
+
+int leftRiemann () {
+     float upper, lower, stepNumber, steps;
+
+
+    cin >> upper >> lower >> stepNumber;
+    
+    steps = (upper - lower) / stepNumber;
+    double heightNum, height, newHeight, area, sum;
+
+    for (int i = 0; i < stepNumber; i++) {
+            
+        heightNum = lower + (steps* (i) );
+        height = pow(heightNum, 2);
+
+        sum += height;
+   
+    }
+    area = sum * steps;
+    cout << area << endl;
+
+    return 0;
+   
+}
+
 int main() {
    
-    rightRiemann();
+    leftRiemann();
 
 }
